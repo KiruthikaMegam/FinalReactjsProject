7037b420e4f0a7fc969341352fb759db67e2d838
Kiruthika <kiruthisekar97@gmail.com>
2026-08-17 14:27:39 +0530

    initial commit

--- src/Context/ProjectContext.jsx
+++ src/Context/ProjectContext.jsx
@@ -31,7 +31,7 @@ const initialProjects = [
   learning:
     "Learned React Router, Context API, state management, CRUD operations, dynamic routing, and responsive web design.",
   github: "https://github.com/KiruthikaMegam/Portfolio",
-  liveDemo: "https://kiruthikamegam.github.io/Portfolio/"
+  liveDemo: "kiruthikamegam.netlify.app"
   },
   {
     id: 2,
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Home.css'
-import Profile from '../assets/Profile.jpeg'
+import Profile from '../assets/Profile.jpg'
 import { NavLink } from 'react-router-dom'
 
 export default function Home() {
